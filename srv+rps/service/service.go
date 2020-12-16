a50a3201ffa40cb94a692c589f1a03752f0d9c4f
Sirok47 <75079540+Sirok47@users.noreply.github.com>
2020-12-16 11:20:32 +0300

    Add files via upload

--- srv+rps/service/service.go
+++ srv+rps/service/service.go
@@ -14,11 +14,12 @@ type TopGames struct {
 
 func simpDigits(a *model.SingleGame) *model.SingleGame{
 	a.Name=a.Name+"(ID=1"
-	for a.ID!=1{
-		for i:=2;i<=a.ID;i++{
-			if a.ID%i==0{
+	b:=a.ID
+	for b!=1{
+		for i:=2;i<=b;i++{
+			if b%i==0{
 				a.Name=a.Name+"*"+strconv.Itoa(i)
-				a.ID/=i
+				b/=i
 				break
 			}
 		}
@@ -26,7 +27,19 @@ func simpDigits(a *model.SingleGame) *model.SingleGame{
 	a.Name=a.Name+")"
 	return a
 }
+/*module TOP_GAMES-interfaces-
 
+go 1.15
+
+require (
+github.com/Sirok47/TOP_GAMES-interfaces- v0.0.0-20201215085411-982006479e4b
+github.com/Sirok47/TOP_GAMES_srv-rps v0.0.0-20201215145027-e5ce92972094
+github.com/gomodule/redigo v1.8.3
+github.com/labstack/echo/v4 v4.1.17
+github.com/lib/pq v1.9.0
+github.com/pkg/errors v0.9.1
+go.mongodb.org/mongo-driver v1.4.4
+)*/
 // NewService is a constructor for creating "TopGames"'s object in service package
 func NewService(rps repository.DBTemplate) *TopGames {
 	return &TopGames{rps}
